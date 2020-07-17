f5df4cb96a311e272eaca4113ba97e75b823ebe1
Anthony Gregis <anthony@thedevtron.com>
2020-07-17 15:59:21 -0500

    [JS] Add 5 variables with different datatypes

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// 5 variables of different datatypes
+
+let varString = "this is a string";
+let varNumber = 0;
+let varArray = [1,2,3,4];
+let varBoolean = true;
+let varObject = {string: "this is a string", number: 0, array: [1,2,3,4], boolean: true}
